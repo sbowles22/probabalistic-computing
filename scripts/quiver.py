fac5665eefcc02e9c48ddbd818950d0356e659ec
Spencer <bowles7@purdue.edu>
2023-08-09 18:33:09 -0400

    Normalized quiver.py script

--- scripts/quiver.py
+++ scripts/quiver.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 from sys import argv
 
 if __name__ == "__main__":
@@ -9,6 +10,15 @@ if __name__ == "__main__":
 
     data_u = [[float(num) for num in line.strip().split(" ")] for line in data_u_raw]
     data_v = [[float(num) for num in line.strip().split(" ")] for line in data_v_raw]
+
+    data_u = np.array(data_u)
+    data_v = np.array(data_v)
+
+    sf = np.sqrt(data_u**2 + data_v**2)
+    sf += 0.000001
+
+    data_u /= sf
+    data_v /= sf
 
     plt.quiver(data_u, data_v)
     plt.savefig("QUIVER.png")
