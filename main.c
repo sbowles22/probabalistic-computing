1cc761601605acc93d14291f4a72baddb5a021e7
Charles Spencer Bowles <bowles7@ececomp2.ecn.purdue.edu>
2023-08-29 11:19:40 -0400

    Add 8.4 mil 8.75 hr accuracy test

--- main.c
+++ main.c
@@ -7,83 +7,48 @@
 #include "network.h"
 #include "utils.h"
 
+#define MIN_GRAPH_SIZE 2
+#define MAX_GRAPH_SIZE 22
+#define MEAN_SPARSITY 0.5
+#define GRAPHS_PER_SIZE 1000
+#define TRIALS_PER_GRAPH 400
+
+#define END_TIME 100.0
+#define STEPS 1000
+
+#define PUMP_RATE 0.9
+#define COUPLING_COEFFICIENT 0.1
+#define NOISE 0.001
 
 int main(int argc, char ** argv) {
 
   Graph* graph;
   Network* network;
   int max_cut_for_graph;
-  for (int i = 0; i < 1; i++) {
-    graph = construct_graph(24);
-    graph = random_mean_sparsity_graph(graph, 0.3);
-
-    // graph = construct_graph(40);
-    // graph = random_mean_sparsity_graph(graph, 0.7);
-    print_adjacency_matrix(*graph);
-    
-    max_cut_for_graph = max_cut(*graph);
-    printf("MAX-CUT: %s%d%s\n", KYEL, max_cut_for_graph, KWHT);
-
-    network = construct_network_from_graph(0.9, 0.1, 0.001, graph, &kraymer_moyal, &euler_maruyama);
-
-    // FILE* u = fopen("u.txt", "w");
-    // FILE* v = fopen("v.txt", "w");
-    // for (double x = -2.0; x < 2.1; x += 0.2) {      
-    //   for (double y = -2.0; y < 2.1; y += 0.2) {
-    //     (network -> c)[0] = x;
-    //     (network -> c)[1] = y;
-    //     (network -> gradient)(network);
-    //     fprintf(u, "%lf ", (network -> dcdt)[0]);
-    //     fprintf(v, "%lf ", (network -> dcdt)[1]);
-    //   }
-    //   fprintf(u, "\n");
-    //   fprintf(v, "\n");
-    // }
-
-    // fclose(u);
-    // fclose(v);
-
-    int cut;
+  
+  for (int size = MIN_GRAPH_SIZE; size <= MAX_GRAPH_SIZE; size++) {
     int success = 0; 
-    int trials = 200;
-    for (int j = 0; j < trials; j++) {
-      // (network -> gradient)(network);
-      // (network -> solver)(network, 0.01);
-      network_run(network, 1000.0, 10000);
-      network_get_partition_array(network);
-      cut = evaluate_cut(*graph, network -> partition_array);
-      // printf("%+d %+d %d\n", (network -> partition_array)[0], (network -> partition_array)[1], cut);
-      // printf("%lf %lf\n", (network -> c)[0], (network -> c)[1]);
-      success += cut == max_cut_for_graph;
+    for (int _gn = 0; _gn < GRAPHS_PER_SIZE; _gn++) {
+      graph = construct_graph(size);
+      graph = random_mean_sparsity_graph(graph, MEAN_SPARSITY);
+
+      // print_adjacency_matrix(*graph);
+      
+      max_cut_for_graph = max_cut(*graph);
+      network = construct_network_from_graph(PUMP_RATE, COUPLING_COEFFICIENT, NOISE, graph, &kraymer_moyal, &euler_maruyama);
+
+      int cut;
+      for (int _tr = 0; _tr < TRIALS_PER_GRAPH; _tr++) {
+        network_run(network, END_TIME, STEPS);
+        network_get_partition_array(network);
+        cut = evaluate_cut(*graph, network -> partition_array);
+        success += cut == max_cut_for_graph;
+      }
+
+      destruct_graph(graph);
+      destruct_network(network);
     }
-    printf("Accuracy: %.2f%%\n", (float) success / trials * 100);
     
-    // for (int j = 0; j < (network -> size); j++) {
-    //   printf("%lf", (network -> c)[j]);
-    //   printf(" %lf\n",  (network -> dcdt)[j]);
-    // }
-
-    destruct_graph(graph);
-    destruct_network(network);
+    printf("%d %.4f\n", size, (float) success / (TRIALS_PER_GRAPH * GRAPHS_PER_SIZE));
   }
-
-  /*
-#pragma omp parallel 
-{
-int size = omp_get_thread_num() + 2;
-Graph graph = *construct_graph(size);
-
-int i;
-int j;
-for (i = 0; i < size; i++) {
-for (j = 0; j < size; j++) {
-graph.edges[i][j] = (i != j);
-}
-}
-
-printf("Size: %2d, Max cut: %4d\n", size, max_cut(graph));
-}
-
-return EXIT_SUCCESS;
-*/
 }
---
+++ main_test.c
@@ -0,0 +1,89 @@
+#include <stdlib.h>
+#include <stdio.h> 
+#include <stdint.h>
+#include <stdbool.h>
+// #include <omp.h>
+#include "graphs.h"
+#include "network.h"
+#include "utils.h"
+
+
+int main(int argc, char ** argv) {
+
+  Graph* graph;
+  Network* network;
+  int max_cut_for_graph;
+  for (int i = 0; i < 1; i++) {
+    graph = construct_graph(24);
+    graph = random_mean_sparsity_graph(graph, 0.3);
+
+    // graph = construct_graph(40);
+    // graph = random_mean_sparsity_graph(graph, 0.7);
+    print_adjacency_matrix(*graph);
+    
+    max_cut_for_graph = max_cut(*graph);
+    printf("MAX-CUT: %s%d%s\n", KYEL, max_cut_for_graph, KWHT);
+
+    network = construct_network_from_graph(0.9, 0.1, 0.001, graph, &kraymer_moyal, &euler_maruyama);
+
+    // FILE* u = fopen("u.txt", "w");
+    // FILE* v = fopen("v.txt", "w");
+    // for (double x = -2.0; x < 2.1; x += 0.2) {      
+    //   for (double y = -2.0; y < 2.1; y += 0.2) {
+    //     (network -> c)[0] = x;
+    //     (network -> c)[1] = y;
+    //     (network -> gradient)(network);
+    //     fprintf(u, "%lf ", (network -> dcdt)[0]);
+    //     fprintf(v, "%lf ", (network -> dcdt)[1]);
+    //   }
+    //   fprintf(u, "\n");
+    //   fprintf(v, "\n");
+    // }
+
+    // fclose(u);
+    // fclose(v);
+
+    int cut;
+    int success = 0; 
+    int trials = 200;
+    for (int j = 0; j < trials; j++) {
+      // (network -> gradient)(network);
+      // (network -> solver)(network, 0.01);
+      network_run(network, 1000.0, 10000);
+      network_get_partition_array(network);
+      cut = evaluate_cut(*graph, network -> partition_array);
+      // printf("%+d %+d %d\n", (network -> partition_array)[0], (network -> partition_array)[1], cut);
+      // printf("%lf %lf\n", (network -> c)[0], (network -> c)[1]);
+      success += cut == max_cut_for_graph;
+    }
+    printf("Accuracy: %.2f%%\n", (float) success / trials * 100);
+    
+    // for (int j = 0; j < (network -> size); j++) {
+    //   printf("%lf", (network -> c)[j]);
+    //   printf(" %lf\n",  (network -> dcdt)[j]);
+    // }
+
+    destruct_graph(graph);
+    destruct_network(network);
+  }
+
+  /*
+#pragma omp parallel 
+{
+int size = omp_get_thread_num() + 2;
+Graph graph = *construct_graph(size);
+
+int i;
+int j;
+for (i = 0; i < size; i++) {
+for (j = 0; j < size; j++) {
+graph.edges[i][j] = (i != j);
+}
+}
+
+printf("Size: %2d, Max cut: %4d\n", size, max_cut(graph));
+}
+
+return EXIT_SUCCESS;
+*/
+}
